423feb1262cebea5e0d8559f7c5a332ffa061771
苏宇泉 <534121271@qq.com>
2019-08-11 15:50:24 +0800

    item

--- itemSystem/src/main/java/com/research/controlller/ItemController.java
+++ itemSystem/src/main/java/com/research/controlller/ItemController.java
@@ -1,5 +1,6 @@
 package com.research.controlller;
 
+import com.research.configuration.JdbcConfigBean;
 import com.research.model.Vo.Item;
 import com.research.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ public class ItemController {
 
     @Autowired
     private ItemService itemService;
+    @Autowired
+    JdbcConfigBean jdbcConfigBean;
 
     /**
      * 对外提供接口服务，查询商品信息
@@ -23,5 +26,10 @@ public class ItemController {
     @GetMapping(value = "item/{id}")
     public Item queryItemById(@PathVariable("id") Long id) {
         return this.itemService.QueryItemById(id);
+    }
+
+    @GetMapping(value = "testconfig")
+    public String testconfig(){
+        return this.jdbcConfigBean.toString();
     }
 }
